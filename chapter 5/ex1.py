ff9ac5a23b827ef350159c7ee44c5037d568ddc2
trang17 <34981161+trang17@users.noreply.github.com>
2021-10-26 16:39:48 +0700

    chapter 5

--- chapter 5/ex1.py
+++ chapter 5/ex1.py
@@ -20,7 +20,19 @@ def compare(a, b):
         result = -1
     return(result)
 
-
+def hypotenuse(a, b):
+    """
+      >>> hypotenuse(3, 4)
+      5.0
+      >>> hypotenuse(12, 5)
+      13.0
+      >>> hypotenuse(7, 24)
+      25.0
+      >>> hypotenuse(9, 12)
+      15.0
+    """
+    cbinh = a**2 + b**2
+    c =  sqrt(cbinh) 
 
 if __name__ == '__main__':
     import doctest
